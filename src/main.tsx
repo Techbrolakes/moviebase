4d5a58a4a39917553cc35c0e3f09bd5e671631dc
Techbrolakes <lekandar11@gmail.com>
2023-04-20 13:42:46 +0100

    Feat: Project Structure Configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <ChakraProvider>
-            <App />
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
         </ChakraProvider>
     </React.StrictMode>,
 );
